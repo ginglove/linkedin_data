bf21f4cd331e493e7cb66aaae643de68e11df2b5
Giang <truonggiangle91@gmail.com>
2021-01-20 17:24:30 +0700

    Update all File and Properties

--- src/automationPackage/Commons/commons.java
+++ src/automationPackage/Commons/commons.java
@@ -1,15 +1,24 @@
 package automationPackage.Commons;
-import org.apache.commons.configuration.ConfigurationException;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
+import com.opencsv.CSVWriterBuilder;
+import com.opencsv.ICSVWriter;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.apache.commons.configuration.CompositeConfiguration;
-import org.apache.commons.configuration.SystemConfiguration;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.event.ConfigurationEvent;
-import org.apache.commons.configuration.event.ConfigurationListener;
-import org.openqa.selenium.WebDriver;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.opencsv.CSVWriter;
+
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class commons {
     public WebDriver driver;
     public commons(WebDriver driver) {
@@ -26,17 +35,145 @@ public class commons {
             System.err.println("Cannot open URL with error "+e);
         }
     }
-    public String Read_Properties_Files(String filePath, String proKey) throws ConfigurationException {
-        String value="";
-        try {
-            CompositeConfiguration config = new CompositeConfiguration();
-            config.addConfiguration(new SystemConfiguration());
-            config.addConfiguration(new PropertiesConfiguration(filePath));
-            value = config.getString(proKey);
+    public void inputIntoElement(String txtInput,String eXpath)
+    {
+        try{
+            WebDriverWait wait = new WebDriverWait(driver,30);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(eXpath)));
+            driver.findElement(By.xpath(eXpath)).sendKeys(txtInput);
+            System.out.println("Input into "+eXpath +"with string "+txtInput+ " successfully");
         }
-        catch(ConfigurationException e) {
-            System.out.println("Cannot get value on Properties file with " + e);
+        catch (ElementNotVisibleException e){
+            System.err.println("No Element is Visible: "+e);
+        }
+        catch(Exception e)
+        {
+            System.err.println("Cannot input into Element due to: "+e);
+        }
+    }
+    public void clickToElement(String eXpath)
+    {
+        try{
+            WebDriverWait wait = new WebDriverWait(driver,30);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(eXpath)));
+            driver.findElement(By.xpath(eXpath)).click();
+            System.out.println("Click "+eXpath+" successfully");
+        }
+        catch (ElementNotVisibleException e){
+            System.err.println("No Element is Visible: "+e);
+        }
+        catch(Exception e)
+        {
+            System.err.println("Cannot input into Element due to: "+e);
+        }
+    }
+    public String Read_Properties_Files(String filePath, String proKey){
+        String value="";
+        try (InputStream input = new FileInputStream(filePath)) {
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+            value=prop.getProperty(proKey);
+            // get the property value and print it out
+            System.out.println(prop.getProperty(value));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.err.println("Cannot get properties with : "+ex);
         }
         return value;
+    }
+    public int numberOfListElement(String eXpath){
+        int iNumberOfListElement=0;
+        try{
+            WebDriverWait wait = new WebDriverWait(driver,30);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(eXpath)));
+            List<WebElement> ls_Element=driver.findElements(By.xpath(eXpath));
+            iNumberOfListElement=ls_Element.size();
+            System.out.println("Number of list "+eXpath+" is "+iNumberOfListElement);
+        }
+        catch (ElementNotVisibleException e){
+            System.err.println("No Element is Visible: "+e);
+        }
+        catch(Exception e)
+        {
+            System.err.println("Cannot input into Element due to: "+e);
+        }
+        return iNumberOfListElement;
+    }
+
+    public String getTextOfElement(String eXpath){
+        String strTextOfElement="";
+        try{
+            WebDriverWait wait = new WebDriverWait(driver,30);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(eXpath)));
+            strTextOfElement=driver.findElement(By.xpath(eXpath)).getText();
+        }
+        catch (ElementNotVisibleException e){
+            System.err.println("No Element is Visible: "+e);
+        }
+        catch(Exception e)
+        {
+            System.err.println("Cannot input into Element due to: "+e);
+        }
+        return strTextOfElement;
+    }
+    public void scrollToEndOfPage()
+    {
+        try{
+            ((JavascriptExecutor)driver).executeScript("scroll(0,800)");
+        }
+        catch(Exception e)
+        {
+            System.err.println("Cannot Scroll due to : "+e);
+        }
+    }
+    public boolean isElementIsVisible(String eXpath)
+    {
+        boolean isVisible=false;
+        isVisible=driver.findElement(By.xpath(eXpath)).isDisplayed();
+        if(isVisible==true)
+        {
+            System.out.println("Element "+eXpath+ " is Visible");
+        }
+        else{
+            System.out.println("Element "+eXpath+ " is not Visible");
+        }
+        return  isVisible;
+    }
+    public boolean isElementPresent(String eXpath) {
+        try {
+            driver.findElement(By.xpath(eXpath));
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+    public void writeDataIntoCSV(String filePath,String[] dataInput) throws IOException {
+        try {
+            // create a write
+            Writer writer = Files.newBufferedWriter(Paths.get(filePath));
+
+            // header record
+            // create a csv writer
+            ICSVWriter csvWriter = new CSVWriterBuilder(writer)
+                    .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                    .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .withEscapeChar(CSVWriter.DEFAULT_ESCAPE_CHARACTER)
+                    .withLineEnd(CSVWriter.DEFAULT_LINE_END)
+                    .build();
+
+            // write header record
+            // write data records
+            csvWriter.writeNext(dataInput);
+
+            // close writers
+            csvWriter.close();
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/automationPackage/demo.java
+++ src/automationPackage/demo.java
@@ -1,30 +1,168 @@
 package automationPackage;
-import org.apache.commons.configuration.ConfigurationException;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import automationPackage.Commons.commons;
-public class demo {
-    public  static FirefoxDriver driver=new FirefoxDriver();
-    public static commons common=new commons(driver);
 
+import java.io.IOException;
+import java.util.Arrays;
+
+public class demo {
+    public static WebDriver driver=new ChromeDriver();
+    public static commons common=new commons(driver);
+    public static String prop_linkedInProfilePage = System.getProperty("user.dir") + "/src/automationPackage/Elements/linkedInProfilePage.properties";
+    public static String prop_linkedInHomePage=System.getProperty("user.dir")+"/src/automationPackage/Elements/linkedInHomePage.properties";
+    public static String filePath=System.getProperty("user.dir")+"/src/automationPackage/TestData/test.csv";
     public static void main(String[] args){
-//        FirefoxOptions options = new FirefoxOptions();
-//        options.setHeadless(true);
-//        FirefoxDriver driver=new FirefoxDriver(options);
+        String chromedriver_linux64=System.getProperty("user.dir")+"/Addons/chromedriver_linux64/chromedriver";
+        System.setProperty("webdriver.chrome.driver", chromedriver_linux64);
+        driver.manage().window().maximize();
+        SignInLinkedInPage();
+        OpenProfileInformation();
+    }
+    public static void SignInLinkedInPage() {
+        common.openURL("https://www.linkedin.com/");
+        //
+        String e_account=common.Read_Properties_Files(prop_linkedInHomePage,"txt_Username");
+        String e_password=common.Read_Properties_Files(prop_linkedInHomePage,"txt_Password");
+        String e_btnSubmit=common.Read_Properties_Files(prop_linkedInHomePage,"btn_Accept");
+        common.inputIntoElement("truonggiangle91@gmail.com",e_account);
+        common.inputIntoElement("o12796303A@",e_password);
+        common.clickToElement(e_btnSubmit);
         try {
-            OpenFireFoxAndRun();
-        } catch (ConfigurationException e) {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public static void OpenFireFoxAndRun() throws ConfigurationException {
-        common.openURL("https://www.linkedin.com/");
-        String e_account=common.Read_Properties_Files("./Elements/linkedInHomePage.properties","txt_Username");
-        WebElement element=driver.findElement(By.xpath(e_account));
-        element.sendKeys("abc@gmail.com");
-        driver.close();
+    public static void OpenProfileInformation() {
+        String prop_linkedInProfilePage = System.getProperty("user.dir") + "/src/automationPackage/Elements/linkedInProfilePage.properties";
+        String e_lnkProfile = common.Read_Properties_Files(prop_linkedInProfilePage, "lnk_Profile");
+        String e_lnkProfileConnections = common.Read_Properties_Files(prop_linkedInProfilePage, "lnk_ProfileConnections");
+        String e_pgNumberOfConnections = common.Read_Properties_Files(prop_linkedInProfilePage, "pg_NumberOfConnections");
+        String e_btnNext = common.Read_Properties_Files(prop_linkedInProfilePage, "btn_Next");
+        String e_lsNumberConnection=common.Read_Properties_Files(prop_linkedInProfilePage, "li_UserConnection");
+
+        common.clickToElement(e_lnkProfile);
+        common.clickToElement(e_lnkProfileConnections);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ((JavascriptExecutor)driver).executeScript("scroll(0,800)");
+        int iNumberofList=common.numberOfListElement(e_pgNumberOfConnections);
+        int iTotalPage= Integer.parseInt(common.getTextOfElement("//ul[@class='artdeco-pagination__pages artdeco-pagination__pages--number']//li["+iNumberofList+"]"));
+        String url = driver.getCurrentUrl();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ((JavascriptExecutor)driver).executeScript("scroll(0,-800)");
+        for(int i=1;i<iTotalPage+1;i++)
+        {
+            if(i==1){}
+            else{
+                url=url+"&page="+i+"";
+            }
+            int iTotalUser= common.numberOfListElement(e_lsNumberConnection);
+            for (int z=1;z<iTotalUser+1;z++)
+            {
+                common.clickToElement("//ul[@class='reusable-search__entity-results-list list-style-none']//li["+z+"]//a[@class='app-aware-link']");
+                getUserInformation();
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                common.openURL(url);
+            }
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ((JavascriptExecutor)driver).executeScript("scroll(0,800)");
+            common.clickToElement(e_btnNext);
+            System.out.println("Click Next iteration : "+i);
+        }
+    }
+    public static void getUserInformation(){
+        String DisplayName="";
+        String UserURL="";
+        String UserMobile="";
+        String UserEmail="";
+        String UserBirthday="";
+        String UserConnected="";
+        String UserUniversityName="";
+        String UserUniversityYear="";
+        String e_lnkSeemore= common.Read_Properties_Files(prop_linkedInProfilePage,"lnk_SeeMore");
+        String e_lnk_UserURL=common.Read_Properties_Files(prop_linkedInProfilePage, "lnk_UserURL");
+        String e_H1UserName=common.Read_Properties_Files(prop_linkedInProfilePage, "h1_NameOfUser");
+        String e_SpanUserMobile=common.Read_Properties_Files(prop_linkedInProfilePage, "span_UserMobile");
+        String e_lnk_Email=common.Read_Properties_Files(prop_linkedInProfilePage,"lnk_UserEmail");
+        String e_Span_BirthDay=common.Read_Properties_Files(prop_linkedInProfilePage,"span_UserBirthDay");
+        String e_Span_Connected=common.Read_Properties_Files(prop_linkedInProfilePage,"span_UserConnected");
+        String e_btn_Close=common.Read_Properties_Files(prop_linkedInProfilePage,"btn_Close");
+        String e_span_Education_Name_Of_University=common.Read_Properties_Files(prop_linkedInProfilePage,"span_Education_Name_Of_University");
+        String e_span_Education_Year=common.Read_Properties_Files(prop_linkedInProfilePage,"span_Education_Year");
+
+        common.clickToElement(e_lnkSeemore);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        DisplayName=common.getTextOfElement(e_H1UserName);
+        UserURL= common.getTextOfElement(e_lnk_UserURL);
+        if(common.isElementPresent(e_SpanUserMobile)==true)
+        {
+            UserMobile= common.getTextOfElement(e_SpanUserMobile);
+        }
+        else{}
+        if(common.isElementPresent(e_lnk_Email)==true)
+        {
+            UserEmail=common.getTextOfElement(e_lnk_Email);
+        }
+        else{}
+        if(common.isElementPresent(e_Span_BirthDay)==true)
+        {
+            UserBirthday=common.getTextOfElement(e_Span_BirthDay);
+        }
+        else{}
+        if(common.isElementPresent(e_Span_Connected)==true)
+        {
+            UserConnected=common.getTextOfElement(e_Span_Connected);
+        }
+        else{}
+        common.clickToElement(e_btn_Close);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ((JavascriptExecutor)driver).executeScript("scroll(0,1300)");
+        ((JavascriptExecutor)driver).executeScript("scroll(0,1300)");
+        if(common.isElementPresent(e_span_Education_Name_Of_University)==true)
+        {
+            UserUniversityName=common.getTextOfElement(e_span_Education_Name_Of_University);
+        }
+        else{}
+        if(common.isElementPresent(e_span_Education_Year)==true)
+        {
+            UserUniversityYear=common.getTextOfElement(e_span_Education_Year);
+            System.out.println(UserUniversityYear);
+        }
+        else{}
+        String[] UserInfor=new String[]{DisplayName,UserURL,UserMobile,UserEmail,UserBirthday,UserConnected,UserUniversityName,UserUniversityYear};
+        try {
+            common.writeDataIntoCSV(filePath,UserInfor);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Arrays.toString(UserInfor));
     }
 }
